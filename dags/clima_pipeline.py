27622afe31402b6ae107d4fb1db16e170bd876a0
MelisaArce <melisaamelia.arce@gmail.com>
2025-07-23 17:29:18 -0300

    se realizo el dag dockeroperator se inicio el nuevo consumer de spark, se comento servicios que no se usaran por el momento

--- dags/clima_pipeline.py
+++ dags/clima_pipeline.py
@@ -1,42 +1,31 @@
-from datetime import datetime, timedelta
 from airflow import DAG
-from airflow.providers.docker.operators.docker import DockerOperator
-from airflow.providers.apache.spark.operators.spark_submit import SparkSubmitOperator
+from airflow.operators.docker_operator import DockerOperator
+from datetime import datetime, timedelta
 
-# default_args = {
-#     'owner': 'mel',
-#     'retries': 1,
-#     'retry_delay': timedelta(minutes=5),
-# }
+default_args = {
+    'owner': 'melisa',
+    'depends_on_past': False,
+    'start_date': datetime(2025, 7, 23),
+    'retries': 1,
+    'retry_delay': timedelta(minutes=5),
+}
 
 with DAG(
-    dag_id='clima_pipeline',
+    dag_id='producer_weather_dag',
     default_args=default_args,
-    description='Orquesta el flujo API > Kafka > Spark > PostgreSQL',
-    start_date=datetime(2025, 4, 17),
-    schedule_interval='0 8,13,20 * * *',  # 08:00, 13:00, 20:00
+    description='Ejecuta el producer de clima 2 veces al día',
+    schedule_interval='0 8,20 * * *',  # 08:00 y 20:00
     catchup=False,
-    tags=['clima', 'kafka', 'spark'],
 ) as dag:
 
-    run_api_producer = DockerOperator(
-        task_id='run_api_producer',
-        image='producer',  # Usa el nombre del servicio del docker-compose
+    run_producer = DockerOperator(
+        task_id='run_weather_producer',
+        image='clima_stack-producer',  # Asegurate que así se llama la imagen
+        container_name='producer_from_airflow',
         api_version='auto',
         auto_remove=True,
         docker_url='unix://var/run/docker.sock',
-        network_mode='airflow_pipeline_net',  # nombre de tu red
-        command="",  # si tu contenedor corre automáticamente, dejalo vacío
-        mounts=[],
+        network_mode='airflow_pipeline_net',  # o el que uses (puede ser tu red de Kafka si es custom)
     )
 
-    run_spark_processing = SparkSubmitOperator(
-    task_id='run_spark_processing',
-    application='/data/spark_kafka_stream.py',  # tu ruta actual
-    conn_id='spark_default',
-    verbose=True,
-    application_args=['--topic', 'weather_data'],  # opcional, si lo usás en el script
-)
-
-
-    run_api_producer >> run_spark_processing
+    run_producer
--- data/spark_kafka_stream.py
+++ data/spark_kafka_stream.py
@@ -1,89 +1,38 @@
 from pyspark.sql import SparkSession
-from pyspark.sql.functions import col, from_json
-from pyspark.sql.types import StructType, StringType, FloatType, IntegerType
-from transformaciones import renombrar_columnas
-import os
+from pyspark.sql.functions import from_json, col
+from pyspark.sql.types import StructType, StringType, FloatType
 
-
-pg_host = os.getenv("PG_HOST")
-pg_port = os.getenv("PG_PORT")
-pg_db = os.getenv("PG_DB")
-pg_user = os.getenv("PG_USER")
-pg_password = os.getenv("PG_PASSWORD")
-pg_table = os.getenv("PG_TABLE_WEATHER")
-spark_master = os.getenv("SPARK_MASTER_URL")
-kafka_broker = os.getenv("KAFKA_BROKER")
-topic = os.getenv("KAFKA_TOPIC")
-
-pg_url = f"jdbc:postgresql://{pg_host}:{pg_port}/{pg_db}"
-
-
+# Crear la sesión de Spark
 spark = SparkSession.builder \
     .master(spark_master) \
     .appName("KafkaToSparkStreaming") \
     .getOrCreate()
 
-# Esquema del mensaje JSON esperado
-schema = StructType() \
-    .add("lat", StringType()) \
-    .add("lon", StringType()) \
-    .add("alt_m", FloatType()) \
-    .add("alt_f", FloatType()) \
-    .add("wx_desc", StringType()) \
-    .add("wx_code", IntegerType()) \
-    .add("wx_icon", StringType()) \
-    .add("temp_c", FloatType()) \
-    .add("temp_f", FloatType()) \
-    .add("feelslike_c", FloatType()) \
-    .add("feelslike_f", FloatType()) \
-    .add("windspd_mph", IntegerType()) \
-    .add("windspd_kmh", IntegerType()) \
-    .add("windspd_kts", IntegerType()) \
-    .add("windspd_ms", FloatType()) \
-    .add("winddir_deg", IntegerType()) \
-    .add("winddir_compass", StringType()) \
-    .add("cloudtotal_pct", IntegerType()) \
-    .add("humid_pct", IntegerType()) \
-    .add("dewpoint_c", FloatType()) \
-    .add("dewpoint_f", FloatType()) \
-    .add("vis_km", FloatType()) \
-    .add("vis_mi", FloatType()) \
-    .add("slp_mb", IntegerType()) \
-    .add("slp_in", FloatType())
+spark.sparkContext.setLogLevel("WARN")
 
-# Leer el stream de Kafka
-df = spark.readStream \
+# Definir el esquema esperado del JSON (ajustalo según tu producer)
+schema = StructType() \
+    .add("location", StringType()) \
+    .add("temperature", FloatType()) \
+    .add("description", StringType()) \
+    .add("timestamp", StringType())
+
+# Leer desde Kafka
+df_kafka = spark.readStream \
     .format("kafka") \
-    .option("kafka.bootstrap.servers", kafka_broker) \
-    .option("subscribe", topic) \
-    .option("startingOffsets", "earliest") \
+    .option("kafka.bootstrap.servers", "broker:29092") \
+    .option("subscribe", "weather_topic") \
     .load()
 
-# Parsear JSON del valor
-weather_df = df.selectExpr("CAST(value AS STRING) as json_string") \
-    .select(from_json(col("json_string"), schema).alias("data")) \
+# Parsear el valor como JSON
+df_valores = df_kafka.selectExpr("CAST(value AS STRING) as json_str") \
+    .select(from_json("json_str", schema).alias("data")) \
     .select("data.*")
 
-
-def proceso_batch(batch_df, batch_id):
-    # Aplicar transformación dentro del batch
-    df_transformado = renombrar_columnas(batch_df)
-    
-    # Guardar en PostgreSQL
-    df_transformado.write \
-        .format("jdbc") \
-        .option("url", pg_url) \
-        .option("dbtable", pg_table) \
-        .option("user", pg_user) \
-        .option("password", pg_password) \
-        .option("driver", "org.postgresql.Driver") \
-        .mode("append") \
-        .save()
-
-        
-query = weather_df.writeStream \
-    .foreachBatch(proceso_batch) \
+# Mostrar en consola
+query = df_valores.writeStream \
     .outputMode("append") \
+    .format("console") \
     .start()
 
 query.awaitTermination()
